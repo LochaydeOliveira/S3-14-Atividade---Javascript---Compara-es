cca263dcde7c3f62d58e4eedc0a54d26659becd5
LochaydeOliveira <lochaydeguerreiro@hotmail.com>
2022-08-10 18:46:55 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// EXERCÍCIO 01
+
+let valor1 = 10
+let valor2 ="10"
+let comparacaoDeValores = valor1 === valor2
+
+
+// EXERCÍCIO 02
+
+let usuario1Altura = 1.80
+let usuario2Altura = 1.75
+let comparacaoDeAlturas = usuario1Altura > usuario2Altura
+
+
+// EXERCÍCIO 03
+
+let valor3 = 10
+let valor4 ="10"
+let comparacao2DeValores = valor3 !== valor4
+
+console.log(comparacaoDeValores)
+console.log(comparacaoDeAlturas)
+console.log(comparacao2DeValores)
